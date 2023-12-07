a0a79570e8a80315d9f27b637d018e3ef76ceb97
DamianL2123 <152392003+DamianL2123@users.noreply.github.com>
2023-12-07 21:26:51 +0100

    Add files via upload

---
+++ Zadanie/src/main/java/com/mycompany/zadanie/Zadanie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zadanie;
+
+/**
+ *
+ * @author intel
+ */
+public class Zadanie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
